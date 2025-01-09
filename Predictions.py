64b128c2d971122c9ef347a7b87af09d031525b7
GitGud48 <liruifeng06@gmail.com>
2025-01-09 10:49:19 -0500

    Update Predictions.py

--- Predictions.py
+++ Predictions.py
@@ -12,23 +12,21 @@ import requests
 import zipfile
 from tqdm.auto import tqdm
 
+#The size of the images are 4*size by 4*size
 size=40
 device ="cpu"
 class_names = ['5-9%','10-14%','15-19%']
 
-class TinyVGG(nn.Module):
-    """
-    Model architecture copying TinyVGG from: 
-    https://poloclub.github.io/cnn-explainer/
-    """
+#Convolution Neural Network
+class body_fat_percentage_model(nn.Module):
     def __init__(self, input_shape: int, hidden_units: int, output_shape: int) -> None:
         super().__init__()
         self.conv_block_1 = nn.Sequential(
             nn.Conv2d(in_channels=input_shape, 
                       out_channels=hidden_units, 
-                      kernel_size=3, # how big is the square that's going over the image?
-                      stride=1, # default
-                      padding=1), # options = "valid" (no padding) or "same" (output has same shape as input) or int for specific number 
+                      kernel_size=3, #size of the convolution square
+                      stride=1,
+                      padding=1),
             nn.ReLU(),
             nn.Conv2d(in_channels=hidden_units, 
                       out_channels=hidden_units,
@@ -37,7 +35,7 @@ class TinyVGG(nn.Module):
                       padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2,
-                         stride=2) # default stride value is same as kernel_size
+                         stride=2)
         )
         self.conv_block_2 = nn.Sequential(
             nn.Conv2d(hidden_units, hidden_units, kernel_size=3, padding=1),
@@ -48,22 +46,17 @@ class TinyVGG(nn.Module):
         )
         self.classifier = nn.Sequential(
             nn.Flatten(),
-            # Where did this in_features shape come from? 
-            # It's because each layer of our network compresses and changes the shape of our input data.
             nn.Linear(in_features=hidden_units*size*size,
                       out_features=output_shape)
         )
     
     def forward(self, x: torch.Tensor):
         x = self.conv_block_1(x)
-        # print(x.shape)
         x = self.conv_block_2(x)
-        # print(x.shape)
         x = self.classifier(x)
-        # print(x.shape)
         return x
 
-model_0 = TinyVGG(input_shape=1, # number of color channels (3 for RGB) 
+model_0 = body_fat_percentage_model(input_shape=1, # number of color channels (3 for RGB, 1 for Grayscale) 
                   hidden_units=3, 
                   output_shape=len(class_names)).to(device)
 
@@ -95,10 +88,10 @@ custom_image_transformed = custom_image_transformed.unsqueeze(dim=0)
 with torch.inference_mode():
     custom_image_pred = model_0(custom_image_transformed.to(device))
 
-# Convert logits -> prediction probabilities (using torch.softmax() for multi-class classification)
+# Convert logits -> prediction probabilities
 custom_image_pred_probs = torch.softmax(custom_image_pred, dim=1)
 
-# Convert prediction probabilities -> prediction labels
+# Convert prediction probabilities to prediction labels
 custom_image_pred_label = torch.argmax(custom_image_pred_probs, dim=1)
 custom_image_pred_class = class_names[custom_image_pred_label.cpu()]
 print(custom_image_pred_class)
